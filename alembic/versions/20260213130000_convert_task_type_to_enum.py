38f4f5aba1d8a83b4255385452069af772cabb2d
Peter Chen <sanpeter992000@gmail.com>
2026-02-13 15:19:09 +0800

    refactor(migration): remove unused sqlalchemy import

--- alembic/versions/20260213130000_convert_task_type_to_enum.py
+++ alembic/versions/20260213130000_convert_task_type_to_enum.py
@@ -8,8 +8,6 @@ Create Date: 2026-02-13 13:00:00.000000
 from typing import Sequence, Union
 
 from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = '20260213130000'
